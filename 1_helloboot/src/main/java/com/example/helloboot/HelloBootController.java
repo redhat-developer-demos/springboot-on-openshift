49ddb0e75adf1e4cb687e068ccbbe477afa4cfc2
Kamesh Sampath <kameshsampath@hotmail.com>
2017-10-08 16:43:12 +0530

    merged debug with 1_helloboot

--- 1_helloboot/src/main/java/com/example/helloboot/HelloBootController.java
+++ 1_helloboot/src/main/java/com/example/helloboot/HelloBootController.java
@@ -1,5 +1,6 @@
 package com.example.helloboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloBootController {
 
     @GetMapping("/whereami")
-    public String whereami() {
-        return String.format("Hello from %s", System.getenv().getOrDefault("HOSTNAME", "localhost"));
+    public String whereami(@Value("${message.prefix}") String prefix) {
+        return String.format("% from %s", prefix, System.getenv().getOrDefault("HOSTNAME", "localhost"));
     }
+
 }
